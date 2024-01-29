0e3fce90915506d6d76a98030a01394b1fe352f5
Swopnil Parajuli <swopnilparajuli01@gmail.com>
2024-01-29 21:28:59 +0545

    Updated

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://bookstoreMERN:firstMERNproject@books-store-mern.oz7nfoo.mongodb.net/books-collection?retryWrites=true&w=majority'
